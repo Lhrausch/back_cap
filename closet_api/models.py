a9b80f4df60671d3d286422bffe0421e988c4ab3
lhrausch <lukerausch@gmail.com>
2022-04-01 14:28:06 -0500

    typo fix

--- closet_api/models.py
+++ closet_api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Clothing(models.Model):
-    type = models.CharField(max_length=32, default="Blank", editable=True)
-    attire = models.CharField(max_length=32, default="Blank", editable=True)
-    description = models.CharField(max_length=32, default="Blank", editable=True)
-    color = models.CharField(max_length=32, default="Blank", editable=True)
-    color_type = models.CharField(max_length=32, default="Blank", editable=True)
+    type = models.CharField(max_length=32, default="Top, Bottom, Shoes", editable=True)
+    attire = models.CharField(max_length=32, default="Sport, Casual, Business", editable=True)
+    description = models.CharField(max_length=32, default="Please Enter a Brief Description", editable=True)
+    color = models.CharField(max_length=32, default="Enter Color Here", editable=True)
+    color_type = models.CharField(max_length=32, default="Enter color based on color type. See Attached Image", editable=True)
